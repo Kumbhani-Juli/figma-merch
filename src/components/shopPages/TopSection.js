b8e2cc104ed17960e803a9f0fa2fe79c65ac6cff
Kumbhani-Juli <julikumbhani5981@gmail.com>
2024-12-04 09:42:02 +0530

    first-commit

--- src/components/shopPages/TopSection.js
+++ src/components/shopPages/TopSection.js
@@ -1,144 +1,107 @@
-import React, { useState } from 'react';
-import '../../assets/css/shopPageCSS/topSection.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleArrowLeft, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
-import Circle from './Circle';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+import "../../assets/css/shopPageCSS/topSection.css";
+import { Link } from "react-router-dom";
+import Circle from "./Circle";
 
 const sliderData = [
-    {
-        id: 1,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg1.webp'),
-        title: "Figma Doodle Socks",
-        slugs: "figma-doodle-socks",
-    },
-    {
-        id: 2,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg2.webp'),
-        slugs: "chunky-glyph-tee",
-        title: "Chunky Glyph Tee"
-    },
-    {
-        id: 3,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg3.webp'),
-        slugs: "chunky-glyph-cap",
-        title: "Chunky Glyph Cap"
-    },
-    {
-        id: 4,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg4.webp'),
-        slugs: "butterfly-tote",
-        title: "Butterfly Tote"
-    },
-    {
-        id: 5,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg5.webp'),
-        title: "Tidy Up Crewneck",
-        slugs: "tidy-up-crewneck",
-    },
-    {
-        id: 6,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg6.webp'),
-        slugs: "all-smile-nalgene",
-        title: "All Smiles Nalgene"
-    },
-    {
-        id: 7,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg7.webp'),
-        slugs: "inkblot-tee",
-        title: "Inkblot Tee"
-    },
-    {
-        id: 8,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg8.webp'),
-        slugs: "tidy-up-crewneck",
-        title: "Tidy Up Crewneck"
-    },
-    {
-        id: 9,
-        imgSrc: require('../../assets/images/shopImgs/topSectionImgs/coverImg9.webp'),
-        slugs: "local-styles-crewneck",
-        title: "Local Styles Crewneck"
-    },
+	{
+		id: 1,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg1.webp"),
+		title: "Figma Doodle Socks",
+		slugs: "figma-doodle-socks",
+	},
+	{
+		id: 2,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg2.webp"),
+		slugs: "chunky-glyph-tee",
+		title: "Chunky Glyph Tee",
+	},
+	{
+		id: 3,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg3.webp"),
+		slugs: "chunky-glyph-cap",
+		title: "Chunky Glyph Cap",
+	},
+	{
+		id: 4,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg4.webp"),
+		slugs: "butterfly-tote",
+		title: "Butterfly Tote",
+	},
+	{
+		id: 5,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg5.webp"),
+		title: "Tidy Up Crewneck",
+		slugs: "tidy-up-crewneck",
+	},
+	{
+		id: 6,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg6.webp"),
+		slugs: "all-smile-nalgene",
+		title: "All Smiles Nalgene",
+	},
+	{
+		id: 7,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg7.webp"),
+		slugs: "inkblot-tee",
+		title: "Inkblot Tee",
+	},
+	{
+		id: 8,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg8.webp"),
+		slugs: "tidy-up-crewneck",
+		title: "Tidy Up Crewneck",
+	},
+	{
+		id: 9,
+		imgSrc: require("../../assets/images/shopImgs/topSectionImgs/coverImg9.webp"),
+		slugs: "local-styles-crewneck",
+		title: "Local Styles Crewneck",
+	},
 ];
 
 const TopSection = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [hoveredIndex, setHoveredIndex] = useState(null); // Maintain hovered index
-    const itemsPerSlide = 3; // Number of items per slide
-    const totalSlides = Math.ceil(sliderData.length / itemsPerSlide); // Total number of slides
+	const [hoverIndex, setHoverIndex] = useState(null);
 
-    // Calculate the width of each slide
-    const slideWidth = 400; // Width of each item
-    const containerWidth = slideWidth * itemsPerSlide; // Width of each slide
-
-    const nextSlide = () => {
-        setCurrentSlide((prevSlide) => {
-            if (prevSlide < sliderData.length - 1) {
-                return prevSlide + 1;
-            }
-            return prevSlide;
-        });
-    };
-
-    const prevSlide = () => {
-        setCurrentSlide((prevSlide) => {
-            if (prevSlide > 0) {
-                return prevSlide - 1;
-            }
-            return prevSlide;
-        });
-    };
-
-    return (
-        <>
-            <div className='shop-top-slider-section-container'>
-                <div className='shop-top-slider-section-content'>
-                    <div
-                        className='shop-top-slider-section-content-inner'
-                        style={{
-                            transform: `translateX(-${currentSlide * containerWidth}px)`,
-                            width: `${containerWidth * totalSlides}px`,
-                            display: 'flex',
-                        }}
-                    >
-                        {Array.from({ length: totalSlides }).map((_, slideIndex) => (
-                            <div
-                                key={slideIndex}
-                                className='shop-top-slider-section-slide'
-                                style={{ width: `${containerWidth}px`, display: 'flex' }}
-                            >
-                                {sliderData.slice(slideIndex * itemsPerSlide, (slideIndex + 1) * itemsPerSlide).map((slide) => (
-                                    <div
-                                        key={slide.id}
-                                        className='shop-top-slider-section-content-box'
-                                        style={{ backgroundImage: `url(${slide.imgSrc})` }}
-                                        onMouseEnter={() => setHoveredIndex(slide.id)}
-                                        onMouseLeave={() => setHoveredIndex(null)}
-                                    >
-                                        <Link to={`/products/${slide?.slugs}`} className='router-link'>
-                                            {hoveredIndex === slide.id && (
-                                                <div className='shop-top-slider-section-content-box-span'>
-                                                    <h3>{slide.title}</h3>
-                                                </div>
-                                            )}
-                                        </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                {/* Slider navigation buttons */}
-                <div className="shop-slider-navigation">
-                    <button className="prev" onClick={prevSlide}><FontAwesomeIcon icon={faCircleArrowLeft} /></button>
-                    <button className="next" onClick={nextSlide}><FontAwesomeIcon icon={faCircleArrowRight} /></button>
-                </div>
-            </div>
-            <Circle circleClass='shop-top-section-circle' />
-        </>
-    );
+	return (
+		<>
+			<div className="shop-top-slider-section-container">
+				<Swiper
+					modules={[Navigation, Pagination]}
+					spaceBetween={20}
+					slidesPerView={4}
+					navigation
+					loop={true}
+					className="shop-top-slider"
+				>
+					{sliderData.map((slide, index) => (
+						<SwiperSlide key={slide.id}>
+							<div
+								className="shop-top-slider-section-content-box"
+								style={{ backgroundImage: `url(${slide.imgSrc})` }}
+								onMouseEnter={() => setHoverIndex(index)}
+								onMouseLeave={() => setHoverIndex(null)}
+							>
+								{hoverIndex === index && (
+									<Link to={`/products/${slide.slugs}`} className="router-link">
+										<div className="shop-top-slider-section-content-box-span">
+											<h3>{slide.title}</h3>
+										</div>
+									</Link>
+								)}
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+			<Circle circleClass="shop-top-section-circle" />
+		</>
+	);
 };
 
 export default TopSection;
